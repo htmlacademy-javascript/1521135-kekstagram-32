bce96eeeceaeb6ac065354a261619d9645a15537
Keks <keks@htmlacademy.ru>
2024-07-10 16:12:10 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/js/main.js
@@ -0,0 +1,27 @@
+import {DESCRIPTIONS, SIMILAR_COMMENTS_COUNT} from './data.js';
+import {getRandomPositiveInteger} from './get-random-positive-integer.js';
+import {createRandomIdFromRangeGenerator} from './create-random-id-from-range-generator.js';
+
+const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
+const generateIdPhotos = createRandomIdFromRangeGenerator(1, SIMILAR_COMMENTS_COUNT);
+const generateIdUrl = createRandomIdFromRangeGenerator(1, SIMILAR_COMMENTS_COUNT);
+
+import {generateCommentsPhoto} from './generate-comments-photo.js';
+
+const createDescriptionPhoto = () => {
+  const randNumber = Math.round(Math.random() * 30);
+  const similarCommentsPhoto = Array.from({length: randNumber}, () => generateCommentsPhoto());
+
+  return {
+    id: generateIdPhotos(),
+    url: `photos/${generateIdUrl()}.jpg`,
+    description: getRandomArrayElement(DESCRIPTIONS),
+    likes: Math.round(Math.random() * 200),
+    comments: similarCommentsPhoto,
+  };
+};
+
+const similarDescriptionPhoto = Array.from({length: SIMILAR_COMMENTS_COUNT}, createDescriptionPhoto);
+
+// eslint-disable-next-line no-console
+console.log(similarDescriptionPhoto);
