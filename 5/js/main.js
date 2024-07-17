aef9e24f6d607623099b08923a8b26c26eb9f29b
Keks <keks@htmlacademy.ru>
2024-07-17 16:35:31 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/main.js
@@ -0,0 +1,26 @@
+import {DESCRIPTIONS, SIMILAR_COMMENTS_COUNT} from './data.js';
+import {getRandomPositiveInteger} from './get-random-positive-integer.js';
+import {createRandomIdFromRangeGenerator} from './create-random-id-from-range-generator.js';
+import {generateCommentsPhoto} from './generate-comments-photo.js';
+import {generateTumbnails} from './generate-thumbnails.js';
+
+const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
+const generateIdPhotos = createRandomIdFromRangeGenerator(1, SIMILAR_COMMENTS_COUNT);
+const generateIdUrl = createRandomIdFromRangeGenerator(1, SIMILAR_COMMENTS_COUNT);
+
+const createDescriptionPhoto = () => {
+  const randNumber = Math.round(Math.random() * 30);
+  const similarCommentsPhoto = Array.from({length: randNumber}, () => generateCommentsPhoto());
+
+  return {
+    id: generateIdPhotos(),
+    url: `photos/${generateIdUrl()}.jpg`,
+    description: getRandomArrayElement(DESCRIPTIONS),
+    likes: Math.round(Math.random() * 200),
+    comments: similarCommentsPhoto,
+  };
+};
+
+const similarDescriptionPhoto = Array.from({length: SIMILAR_COMMENTS_COUNT}, createDescriptionPhoto);
+
+generateTumbnails(similarDescriptionPhoto);
